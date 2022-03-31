eb1eb39b475d25de07c8447ddc95a078af0b03d3
mcweek <qq2080782895@outlook.com>
2022-03-31 22:54:41 +0800

    三维散点图实现

--- PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1.py
@@ -3,11 +3,11 @@ import matplotlib.pyplot as plt
 import dataset
 import mpl_toolkits.mplot3d as  A3D
 #*******************************
-[x,y] = dataset.get_beans(5)
+[x,y,z] = dataset.get_beans(100)
 fig = plt.figure("mark")
 ax = A3D.Axes3D(fig)
 ax.set_xlabel("x-bar")
 ax.set_ylabel("y-bar")
 ax.set_zlabel("z-bar")
-
+ax.scatter(x,y,z)
 plt.show()
--- PythonApplication1/dataset.py
+++ PythonApplication1/dataset.py
@@ -2,9 +2,11 @@ import numpy as np
 
 def get_beans(counts):
 	xs = np.random.rand(counts)
+	ys = np.random.rand(counts)
+	ys = np.sort(ys)
 	xs = np.sort(xs)
-	ys = []
+	zs = []
 	for x in range(counts):
-		ys.append(xs[x]*1.2+np.random.rand()/10)
-	return xs,ys
+		zs.append(xs[x]*1.2+ys[x]*2.5+np.random.rand()/10)
+	return xs,ys,zs
 
