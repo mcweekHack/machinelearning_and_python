6bd88ad8fb7ada87ad96df43c142760355f0e14f
mcweek <qq2080782895@outlook.com>
2022-04-01 12:05:19 +0800

    模型调整中

--- PythonApplication1/FunctionGather.py
+++ PythonApplication1/FunctionGather.py
@@ -19,11 +19,11 @@ def equation_Create(x1,y1,z1):#两参数代价函数生成(适用于过零点的数据集)
     val1_val2 = sum(x1*y1)
     val3_val1 = sum(z1*x1)
     val3_val2 = sum(z1*y1)
-    xs = np.arange(0,5,1)
-    ys = np.arange(0,5,1)
+    xs = np.arange(-5,5,1)
+    ys = np.arange(-5,5,1)
     xs,ys = np.meshgrid(xs, ys)
     length = x1.shape
-    equation = (xs*xs*val1+2*xs*ys*val1_val2+ys*ys*val2-2*xs*val3_val1-2*ys*val3_val2+val3)/length[0]
+    equation = (xs*xs*val1+2*xs*ys*val1_val2+ys*ys*val2-2*xs*val3_val1-2*ys*val3_val2+val3)/(2*length[0])
     fig = plt.figure("Cost function")
     axe = A3D.Axes3D(fig)
     axe.set_xlabel("x1-value")
@@ -31,3 +31,36 @@ def equation_Create(x1,y1,z1):#两参数代价函数生成(适用于过零点的数据集)
     axe.set_zlabel("disparity")
     axe.plot_surface(xs,ys,equation,rstride = 1,cstride = 1,cmap = cm.coolwarm)
     return fig
+def equation_task(x1,y1,z1,Acceptance):#返回两个未知参数的值,Acceptance表示精度
+    Alpha = 0.05
+    val1 = sum(x1*x1)
+    val2 = sum(y1*y1)
+    val3 = sum(z1*z1)
+    val1_val2 = sum(x1*y1)
+    val3_val1 = sum(z1*x1)
+    val3_val2 = sum(z1*y1)
+    length = x1.shape
+    xs = 0
+    ys = 0
+    track1 = 0
+    traack2 = 0
+    while  abs((xs*xs*val1+2*xs*ys*val1_val2+ys*ys*val2-2*xs*val3_val1-2*ys*val3_val2+val3)/(2*length[0]))>Acceptance:
+        track = xs - Alpha*(xs*val1+ys*val1_val2-val3_val1)/length[0]
+        track2 = ys - Alpha*(ys*val2+xs*val1_val2-val3_val2)/length[0]
+        xs = track1
+        ys = track2
+        print("analysing...")
+    print("The model's parameter:",xs,ys)
+    return xs,ys
+def map_Create_Fun(x2,y2):#画出一个二元1次方程,即验证结果
+    xs = np.arange(-5,5,1)
+    ys = np.arange(-5,5,1)
+    xs,ys = np.meshgrid(xs,ys)
+    equation = xs*x2+ys*y2
+    fig = plt.figure("Prediction function")
+    axe = A3D.Axes3D(fig)
+    axe.set_xlabel("x1-value")
+    axe.set_ylabel("x2-value")
+    axe.set_zlabel("res-value")
+    axe.plot_surface(xs,ys,equation,rstride = 1,cstride = 1)
+    return 0
--- PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1.py
@@ -5,7 +5,9 @@ import datam
 import FunctionGather as Fun
 from matplotlib import cm
 #*******************************
-[x,y,z] = datam.Get_data(10,0)
-Fun.Spot_map_Create(x,y,z)
-Fun.equation_Create(x,y,z)
-plt.show()
+[x,y,z] = datam.Get_data(10,0)#生成并获取数据集
+Fun.Spot_map_Create(x,y,z)#生成数据集的散点图
+Fun.equation_Create(x,y,z)#生成代价函数的散点图
+a,b = Fun.equation_task(x,y,z,0.5)#计算并返回预测函数的两个模型参数
+Fun.map_Create_Fun(a,b)
+plt.show()#输出
