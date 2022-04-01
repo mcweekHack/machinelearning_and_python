bccdd221e5f5dbc0cf49bc083bb5858fc7d96e88
mcweek <qq2080782895@outlook.com>
2022-04-01 08:02:40 +0800

    代码结构调整

---
+++ PythonApplication1/FunctionGather.py
@@ -0,0 +1,4 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import mpl_toolkits.mplot3d as  A3D
+#------------------------------------------------
--- PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1.py
@@ -1,6 +1,5 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import dataset
 import mpl_toolkits.mplot3d as  A3D
 import datam
 #*******************************
