5495b721754c452da027e0a0a20c375d91750825
mcweek <qq2080782895@outlook.com>
2022-04-02 16:29:41 +0800

    高维数据集建立

--- PythonApplication1/datam.py
+++ PythonApplication1/datam.py
@@ -1,6 +1,6 @@
 import numpy as np
-
-def Get_data(counts,value):#生成数据集
+#生成数据集(二元一次函数)
+def Get_data(counts,value):
     xs = np.random.rand(counts*counts)
     ys = []
     zs = []
@@ -10,3 +10,20 @@ def Get_data(counts,value):#生成数据集
     ys = np.array(ys)
     zs = np.array(zs)
     return xs,ys,zs
+#生成高维数据集
+def Get_Pro_data(num,Unknown,value):
+    data_ = []
+    part = np.array([])
+    theta = np.random.rand(Unknown)
+    value = 0
+    for i in range(Unknown):
+        for i2 in range(num):
+            val = np.random.rand()
+            part = np.append(part,val)
+        data_.append(part)
+    data_ = np.array(data_)
+    res = data_.transpose()@theta.transpose
+    res = res.transpose()
+    data_ = np.append(data_,res,axis = 0)
+    data_ = data_.transpose()
+    return num, data_, theta
